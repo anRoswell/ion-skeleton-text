17f2d0dddd7a164973ffa1e81f15784d51f7db73
Alfonso Navarro <alfonso.navarro@syspotec.com>
2019-10-08 10:18:51 -0500

    Ejemplo plugin funcionando

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: "app-home",
+	templateUrl: "home.page.html",
+	styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+	data: any
 
-  constructor() {}
+	constructor() {}
 
+	ionViewWillEnter() {
+		setTimeout(() => {
+			this.data = {
+				heading: "Normal text",
+				para1: "Lorem ipsum dolor sit amet, consectetur",
+				para2: "adipiscing elit.",
+			}
+		}, 5000)
+	}
 }
